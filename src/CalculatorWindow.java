a3175ea654e1d9d486610d7429c21cc9d1b31071
katurina <bird.katyrina@gmail.com>
2017-02-03 00:04:21 +0300

    all buttons are placed

--- src/CalculatorWindow.java
+++ src/CalculatorWindow.java
@@ -12,7 +12,7 @@ public class CalculatorWindow extends JFrame {
     private JButton multiplication = new JButton("*");
     private JButton divicion = new JButton("/");
     private JPanel panel = new JPanel();
-    private JTextField text = new JTextField();
+    private static JTextField text = new JTextField();
 
     public CalculatorWindow() {
         super("Calculator"); //Заголовок окна
@@ -21,11 +21,13 @@ public class CalculatorWindow extends JFrame {
 
         int i = 60;
         int j = 300 / 4;
-        summ.setBounds(300-j, i, i, 60);
-        subtraction.setBounds(300-j,i*2,i,i);
-        multiplication.setBounds(300-j, i*3,i,i);
-        divicion.setBounds(300-j,i*4,i,i);
-        text.setBounds(0,0,300,60);
+        summ.setBounds(300 - j, i, i, 60);
+        subtraction.setBounds(300 - j, i * 2, i, i);
+        multiplication.setBounds(300 - j, i * 3, i, i);
+        divicion.setBounds(300 - j, i * 4, i, i);
+        text.setBounds(0, 0, 300, 60);
+
+        ArrayList<JButton> buttons = new ArrayList();
 
 
         panel.add(text);
@@ -33,35 +35,51 @@ public class CalculatorWindow extends JFrame {
         panel.add(subtraction);
         panel.add(multiplication);
         panel.add(divicion);
-        int s=1,s2;
-        ArrayList<JButton> buttons = new ArrayList();
+        int s = 1, s2;
+
         JButton button = new JButton("0");
-        button.setBounds(0,i*4,i,i);
-        buttons.add(0,button);
+        button.setBounds(0, i * 4, i, i);
+        buttons.add(0, button);
         panel.add(button);
         for (int s1 = 1; s1 < 4; s1++) {
             for (s2 = 0; s2 < 3; s2++) {
                 button = new JButton(Integer.toString(s));
-                buttons.add(s,button);
-                button.setBounds(s2*i,s1*i , i, i);
+                buttons.add(s, button);
+                button.setBounds(s2 * i, s1 * i, i, i);
                 panel.add(button);
                 s++;
             }
         }
         button = new JButton(",");
-        button.setBounds(i,i*4,i,i);
-        buttons.add(10,button);
+        button.setBounds(i, i * 4, i, i);
+        buttons.add(10, button);
         panel.add(button);
 
         button = new JButton("=");
-        button.setBounds(i*2,i*4,i,i);
-        buttons.add(11,button);
+        button.setBounds(i * 2, i * 4, i, i);
+        buttons.add(11, button);
         panel.add(button);
 
         setContentPane(panel);
+
+
+        listenButton(buttons);
+
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //это нужно для того чтобы при
         //закрытии окна закрывалась и программа,
         //иначе она останется висеть в процессах
     }
+
+    static void listenButton(ArrayList<JButton> button) {
+
+
+        for (JButton buttonInMethod : button) {
+            buttonInMethod.addActionListener(e -> {
+                String textWriter = buttonInMethod.getText();
+                text.setText(textWriter);
+            });
+        }
+    }
+
 }

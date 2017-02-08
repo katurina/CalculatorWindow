dfc7b2e612cfd48deebded8ed1268406e0306fc0
katurina <bird.katyrina@gmail.com>
2017-02-08 20:42:08 +0300

    work correctly

--- src/CalculatorWindow.java
+++ src/CalculatorWindow.java
@@ -107,59 +107,81 @@ public class CalculatorWindow extends JFrame {
             if (text.isEmpty()) {
                 return "Input expression";
             }
-            return Double.toString(eval_(text, 0, text.length()));
+            return Double.toString(eval(text));
         }
 
-//        private static int eval(String expr, int from, int to) {
-//            if (expr.charAt(from) == '(') {
-//                return eval(expr, from + 1, to - 1);
-//            } else {
-//                int pos = from;
-//                while (pos < to) {
-//                    if (Character.isDigit(expr.charAt(pos))) {
-//                        pos++;
-//                    } else {
-//                        int leftOperand = Integer.valueOf(expr.substring(from, pos));
-//                        char operation = expr.charAt(pos);
-//                        int rightOperand = eval(expr, pos + 1, to);
-//                        switch (operation) {
-//                            case '+':
-//                                return leftOperand + rightOperand;
-//                            case '-':
-//                                return leftOperand - rightOperand;
-//                            case '*':
-//                                return leftOperand * rightOperand;
-//                            case '/':
-//                                return leftOperand / rightOperand;
-//                        }
-//                    }
-//                }
-//                return Integer.valueOf(expr.substring(from, to));
-//            }
-//        }
+        static boolean isDelim(char c) { // тру если пробел
+            return c == ' ';
+        }
 
-        private static double eval_(String expr, int from, int to) {
-            int pos = from;
-            while (pos < to) {
-                if (Character.isDigit(expr.charAt(pos)) || expr.charAt(pos) == '.') {
-                    pos++;
+        static boolean isOperator(char c) { // возвращяем тру если один из символов ниже
+            return c == '+' || c == '-' || c == '*' || c == '/' || c == '%';
+        }
+
+        static int priority(char op) {
+            switch (op) { // при + или - возврат 1, при * / % 2 иначе -1
+                case '+':
+                case '-':
+                    return 1;
+                case '*':
+                case '/':
+                case '%':
+                    return 2;
+                default:
+                    return -1;
+            }
+        }
+
+        static void processOperator(LinkedList<Integer> st, char op) {
+            int r = st.removeLast(); // выдёргиваем из упорядоченного листа последний элемент
+            int l = st.removeLast(); // также
+            switch (op) { // выполняем действие между l и r в зависимости от оператора в кейсе и результат валим в st
+                case '+':
+                    st.add(l + r);
+                    break;
+                case '-':
+                    st.add(l - r);
+                    break;
+                case '*':
+                    st.add(l * r);
+                    break;
+                case '/':
+                    st.add(l / r);
+                    break;
+                case '%':
+                    st.add(l % r);
+                    break;
+            }
+        }
+
+        public static int eval(String s) {
+            LinkedList<Integer> st = new LinkedList<Integer>(); // сюда наваливают цифры
+            LinkedList<Character> op = new LinkedList<Character>(); // сюда опрераторы и st и op в порядке поступления
+            for (int i = 0; i < s.length(); i++) { // парсим строку с выражением и вычисляем
+                char c = s.charAt(i);
+                if (isDelim(c))
+                    continue;
+                if (c == '(')
+                    op.add('(');
+                else if (c == ')') {
+                    while (op.getLast() != '(')
+                        processOperator(st, op.removeLast());
+                    op.removeLast();
+                } else if (isOperator(c)) {
+                    while (!op.isEmpty() && priority(op.getLast()) >= priority(c))
+                        processOperator(st, op.removeLast());
+                    op.add(c);
                 } else {
-                    double leftOperand = Double.valueOf(expr.substring(from, pos));
-                    char operation = expr.charAt(pos);
-                    double rightOperand = eval_(expr, pos + 1, to);
-                    switch (operation) {
-                        case '+':
-                            return leftOperand + rightOperand;
-                        case '-':
-                            return leftOperand - rightOperand;
-                        case '*':
-                            return leftOperand * rightOperand;
-                        case '/':
-                            return leftOperand / rightOperand;
-                    }
+                    String operand = "";
+                    while (i < s.length() && Character.isDigit(s.charAt(i)))
+                        operand += s.charAt(i++);
+                    --i;
+                    st.add(Integer.parseInt(operand));
                 }
             }
-            return Double.valueOf(expr.substring(from, to));
+            while (!op.isEmpty())
+                processOperator(st, op.removeLast());
+            return st.get(0);  // возврат результата
         }
     }
 }
